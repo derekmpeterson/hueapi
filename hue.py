f423110a41ebbd400e03da2768a4b788584bf99d
Derek Peterson <dmp013@gmail.com>
2013-03-07 10:39:20 -0700

    Brought up to pep8 standards.

--- hue.py
+++ hue.py
@@ -7,6 +7,7 @@ import time
 import random
 from datetime import datetime
 
+
 class Hue:
     ip_address = None
     api_key = None
@@ -16,7 +17,7 @@ class Hue:
         self.api_key = api_key
 
     def get_hex(self, hsv_h, hsv_s, hsv_v):
-        hsv = HSVColor(hsv_h=hsv_h/200, hsv_s=hsv_s/254, hsv_v=hsv_v/254)
+        hsv = HSVColor(hsv_h=hsv_h / 200, hsv_s=hsv_s / 254, hsv_v=hsv_v / 254)
         rgb = hsv.convert_to('rgb')
         return rgb.get_rgb_hex()
 
@@ -39,7 +40,10 @@ class Hue:
 
     def get_light(self, light):
         state = self.api_call("lights/%d/" % light)['state']
-        state['hex_color'] = self.get_hex(float(state['hue']), float(state['sat']), float(state['bri']))
+        state['hex_color'] = self.get_hex(
+            float(state['hue']),
+            float(state['sat']),
+            float(state['bri']))
 
         return state
 
@@ -70,11 +74,13 @@ class Hue:
         try:
             if data is None:
                 r = requests.get(
-                    "http://%s/api/%s/%s" % (self.ip_address, self.api_key, endpoint),
+                    ("http://%s/api/%s/%s"
+                     % (self.ip_address, self.api_key, endpoint)),
                     timeout=1.0)
             else:
                 r = requests.put(
-                    "http://%s/api/%s/%s" % (self.ip_address, self.api_key, endpoint),
+                    ("http://%s/api/%s/%s"
+                     % (self.ip_address, self.api_key, endpoint)),
                     data=data,
                     timeout=1.0)
             return r.json()
